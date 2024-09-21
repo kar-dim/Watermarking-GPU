8f3f2b006d0697c2055b4e3547ed1ce4b3e27daa
Mimis <dim_kar@outlook.com>
2024-09-21 22:37:07 +0300

    fix video pt2, remove openmp (broken for now)

--- Watermark_GPU/Watermark_GPU.cpp
+++ Watermark_GPU/Watermark_GPU.cpp
@@ -10,6 +10,7 @@
 #include <exception>
 #include <format>
 #include <iostream>
+//#include <omp.h>
 #include <string>
 #include <vector>
 
@@ -35,6 +36,10 @@ int main(void)
 		cout << "Could not load opencl configuration file\n";
 		exit_program(EXIT_FAILURE);
 	}
+
+	//omp_set_num_threads(omp_get_max_threads());
+//#pragma omp parallel for
+	//for (int i = 0; i < 24; i++) { }
 
 	af::setDevice(inir.GetInteger("options", "opencl_device", 0));
 	af::info();
@@ -174,7 +179,6 @@ int test_for_image(const cl::Device& device, const cl::Program& program_nvf, con
 		//}
 		cout << "Successully saved to disk\n";
 	}
-
 	return EXIT_SUCCESS;
 }
 
@@ -335,11 +339,8 @@ int test_for_video(const cl::Device& device, const cl::Program& program_nvf, con
 		string video_compressed_path = inir.Get("paths", "video_compressed", "NO_VIDEO");
 		CImgList<unsigned char>video_cimg_w = CImgList<unsigned char>::get_load_video(video_compressed_path.c_str(), 0, frames - 1);
 		std::vector<af::array> watermarked_frames(frames);
-#pragma omp parallel for
-		for (int i = 0; i < frames; i++) {
+		for (int i = 0; i < frames; i++)
 			watermarked_frames[i] = Utilities::cimg_yuv_to_afarray<unsigned char>(video_cimg_w.at(i));
-		}
-
 		realtime_detection(watermark_obj, watermarked_frames, frames, display_frames, frame_period, show_fps);
 	}
 	return EXIT_SUCCESS;
