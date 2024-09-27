e3e4629320d11ef41b514908a5b151f8712b3513
Mimis <dim_kar@outlook.com>
2024-09-27 23:25:34 +0300

    rename some files

--- Watermark_GPU/main.cpp
+++ Watermark_GPU/main.cpp
@@ -1,8 +1,8 @@
 ﻿#include "cimg_init.h"
+#include "main_utils.hpp"
 #include "opencl_init.h"
 #include "Utilities.hpp"
 #include "Watermark.hpp"
-#include "Watermark_GPU.hpp"
 #include <af/opencl.h>
 #include <cstdlib>
 #include <cstring>
