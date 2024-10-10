1c67ea3c60f58ecf3eba1a25e394f702f0112789
Mimis <dim_kar@outlook.com>
2024-10-10 14:22:51 +0300

    faster NVF calculation

--- Watermark_GPU/kernels.cuh
+++ Watermark_GPU/kernels.cuh
@@ -1,19 +1,20 @@
 #pragma once
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
+#include "stdio.h"
 
 //constant array used for optimizing share memory accesses for Rx
 //Helps with reducing the local memory required for each block for Rx arrays from 4096 to 2304
 __constant__ int RxMappings[64] =
 {
-    0,  1,  2,  3,  4,  5,  6,  7,
-    1,  8,  9,  10, 11, 12, 13, 14,
-    2,  9,  15, 16, 17, 18, 19, 20,
-    3,  10, 16, 21, 22, 23, 24, 25,
-    4,  11, 17, 22, 26, 27, 28, 29,
-    5,  12, 18, 23, 27, 30, 31, 32,
-    6,  13, 19, 24, 28, 31, 33, 34,
-    7,  14, 20, 25, 29, 32, 34, 35
+	0,  1,  2,  3,  4,  5,  6,  7,
+	1,  8,  9,  10, 11, 12, 13, 14,
+	2,  9,  15, 16, 17, 18, 19, 20,
+	3,  10, 16, 21, 22, 23, 24, 25,
+	4,  11, 17, 22, 26, 27, 28, 29,
+	5,  12, 18, 23, 27, 30, 31, 32,
+	6,  13, 19, 24, 28, 31, 33, 34,
+	7,  14, 20, 25, 29, 32, 34, 35
 };
 
 template<int p, int pSquared = p * p, int pad = p / 2>
@@ -25,27 +26,20 @@ __global__ void nvf(cudaTextureObject_t texObj, float* nvf, const unsigned int w
 	if (x >= width || y >= height)
 		return;
 
-	int i, j, k = 0;
-	float mean = 0.0f, variance = 0.0f, localMeanDiff;
-	//maximum local values size is 81 for a 9x9 block
-	float localValues[pSquared];
-	for (i = y - pad; i <= y + pad; i++)
+	float sum = 0.0f, sumSq = 0.0f;
+	for (int i = y - pad; i <= y + pad; i++)
 	{
-		for (j = x - pad; j <= x + pad; j++) 
+		for (int j = x - pad; j <= x + pad; j++)
 		{
-			localValues[k] = tex2D<float>(texObj, i, j);
-			mean += localValues[k];
-			k++;
+			float val = tex2D<float>(texObj, i, j);
+			sum += val;
+			sumSq += val * val;
 		}
 	}
-	mean /= pSquared;
-	for (i = 0; i < pSquared; i++) 
-	{
-		localMeanDiff = localValues[i] - mean;
-		variance += localMeanDiff * localMeanDiff;
-	}
+	float mean = sum / pSquared;
+	float variance = (sumSq / pSquared) - (mean * mean);
 	//calculate mask and write pixel value
-	nvf[(x * height) + y] = variance / ((pSquared - 1) + variance);
+	nvf[(x * height) + y] = variance / (1 + variance);
 }
 
 __global__ void me_p3(cudaTextureObject_t texObj, float* Rx, float* rx, const unsigned int width, const unsigned int paddedWidth, const unsigned int height);
