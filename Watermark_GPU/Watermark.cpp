75e6645a74ac1581b9386b156605584b543b7af3
Mimis <dim_kar@outlook.com>
2025-02-07 20:21:56 +0200

    use af::norm to simplify watermark strength expression

--- Watermark_GPU/Watermark.cpp
+++ Watermark_GPU/Watermark.cpp
@@ -159,7 +159,7 @@ af::array Watermark::makeWatermark(const af::array& inputImage, const af::array&
 		computePredictionErrorMask(inputImage, errorSequence, coefficients, ME_MASK_CALCULATION_REQUIRED_YES) :
 		computeCustomMask();
 	const af::array u = mask * randomMatrix;
-	watermarkStrength = strengthFactor / sqrt(af::sum<float>(af::pow(u, 2)) / (inputImage.elements()));
+	watermarkStrength = strengthFactor / (af::norm(u) / sqrt(inputImage.elements()));
 	return af::clamp(outputImage + (u * watermarkStrength), 0, 255);
 }
 
