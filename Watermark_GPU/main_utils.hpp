0bbef34a6be9b2aa96342d596578e702a064c774
Mimis <dim_kar@outlook.com>
2025-03-16 19:18:14 +0200

    some code refactors

--- Watermark_GPU/main_utils.hpp
+++ Watermark_GPU/main_utils.hpp
@@ -3,7 +3,9 @@
 #include <arrayfire.h>
 #include <cstdint>
 #include <cuda_runtime.h>
+#include <functional>
 #include <INIReader.h>
+#include <stdio.h>
 #include <string>
 
 extern "C" {
@@ -27,3 +29,4 @@ AVCodecContext* openDecoderContext(const AVCodecParameters* params);
 bool receivedValidVideoFrame(AVCodecContext* inputDecoderCtx, AVPacket* packet, AVFrame* frame, const int videoStreamIndex);
 void embedWatermarkFrame(af::array& inputFrame, af::array& watermarkedFrame, const int height, const int width, const int watermarkInterval, int& framesCount, AVFrame* frame, uint8_t* frameFlatPinned, FILE* ffmpegPipe, const Watermark& watermarkObj);
 void detectFrameWatermark(af::array& inputFrame, const int height, const int width, const int watermarkInterval, int& framesCount, AVFrame* frame, uint8_t* frameFlatPinned, const Watermark& watermarkObj);
+void processFrames(AVFormatContext* formatCtx, AVCodecContext* decoderCtx, const int videoStreamIndex, int& framesCount, std::function<void(AVFrame*, int&)> processFrame);
