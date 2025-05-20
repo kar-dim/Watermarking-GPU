6d466e394827551138e2af95feea3611c2ae325a
Mimis <dim_kar@outlook.com>
2025-05-21 00:18:39 +0300

    CUDA port: optimize nvf/neighbors, minor speedup

--- Watermark_GPU/kernels/nvf.hpp
+++ Watermark_GPU/kernels/nvf.hpp
@@ -10,76 +10,36 @@ __kernel void nvf(__read_only image2d_t image,
 	//image2d is transposed, so we read the opposite dimensions
 	const int width = get_image_height(image), height = get_image_width(image);
 	const int x = get_global_id(1), y = get_global_id(0);
+    const int localId = get_local_id(1) * get_local_size(0) + get_local_id(0);
 	//p is supplied at compile-time to allow the compiler to optimize more efficiently
 	const int pSquared = p * p;
 	const int pad = p / 2;
-    const int localY = get_local_id(1);
-    const int localX = get_local_id(0);
-    const int shX = localY + pad;
-    const int shY = localX + pad;
+    const int sharedSize = 16 + (2 * pad);
 
-    //load current pixel value
-    region[shY][shX] = read_imagef(image, sampler, (int2)(y, x)).x;
-
-    // Load the padded regions (only edge threads)
-    if (localX == 0)
+    //load cooperatively the region for this 16 x 16 block
+    for (int i = localId; i < sharedSize * sharedSize; i += get_local_size(0) * get_local_size(1))
     {
-        for (int i = pad; i > 0; i--)
-            region[shY - i][shX] = read_imagef(image, sampler, (int2)(y - i, x)).x;
-    }
-    if (localX == 15)
-    {
-        for (int i = pad; i > 0; i--)
-            region[shY + i][shX] = read_imagef(image, sampler, (int2)(y + i, x)).x;
-    }
-    if (localY == 0)
-    {
-        for (int i = pad; i > 0; i--)
-            region[shY][shX - i] = read_imagef(image, sampler, (int2)(y, x - i)).x;
-    }
-    if (localY == 15)
-    {
-        for (int i = pad; i > 0; i--)
-            region[shY][shX + i] = read_imagef(image, sampler, (int2)(y, x + i)).x;
-    }
-
-    // Load the corners of the padded region (only edge threads)
-    if (localX == 0 && localY == 0)
-    {
-        for (int i = -1; i >= -pad; i--)
-            for (int j = -1; j >= -pad; j--)
-                region[shY + i][shX + j] = read_imagef(image, sampler, (int2)(y + i, x + j)).x;
-    }
-    if (localX == 15 && localY == 15)
-    {
-        for (int i = 1; i <= pad; i++)
-            for (int j = 1; j <= pad; j++)
-                region[shY + i][shX + j] = read_imagef(image, sampler, (int2)(y + i, x + j)).x;
-    }
-    if (localX == 0 && localY == 15)
-    {
-        for (int i = -1; i >= -pad; i--)
-            for (int j = 1; j <= pad; j++)
-                region[shY + i][shX + j] = read_imagef(image, sampler, (int2)(y + i, x + j)).x;
-    }
-    if (localX == 15 && localY == 0)
-    {
-        for (int i = 1; i <= pad; i++)
-            for (int j = -1; j >= -pad; j--)
-                region[shY + i][shX + j] = read_imagef(image, sampler, (int2)(y + i, x + j)).x;
+        const int tileRow = i / sharedSize;
+        const int tileCol = i % sharedSize;
+        const int globalX =  get_group_id(1) * get_local_size(1) + tileCol - pad;
+        const int globalY = get_group_id(0) * get_local_size(0) + tileRow - pad;
+        region[tileRow][tileCol] = read_imagef(image, sampler, (int2)(globalY, globalX)).x;
     }
     barrier(CLK_LOCAL_MEM_FENCE);
 
-
     if (y >= height || x >= width)
         return;
+
+	// Local (shared memory) coordinates for center pixel
+    const int centerCol = get_local_id(1) + pad;
+    const int centerRow = get_local_id(0) + pad;
 
 	float sum = 0.0f, sumSq = 0.0f;
 	for (int i = -pad; i <= pad; i++)
 	{
 		for (int j = -pad; j <= pad; j++)
 		{
-			float pixelValue = region[shY + i][shX + j];
+			float pixelValue = region[centerRow + i][centerCol + j];
 			sum += pixelValue;
 			sumSq += pixelValue * pixelValue;
 		}
--- Watermark_GPU/kernels/scaled_neighbors_p3.hpp
+++ Watermark_GPU/kernels/scaled_neighbors_p3.hpp
@@ -10,51 +10,36 @@ __kernel void scaled_neighbors_p3(
 {
     const int x = get_global_id(1);
     const int y = get_global_id(0);
-    const int localX = get_local_id(0);
-	const int localY = get_local_id(1);
-    const int shX = localY + 1;
-    const int shY = localX + 1;
+    const int localId = get_local_id(1) * get_local_size(0) + get_local_id(0);
     const sampler_t sampler = CLK_NORMALIZED_COORDS_FALSE | CLK_ADDRESS_CLAMP_TO_EDGE | CLK_FILTER_NEAREST;
     //image2d is transposed, so we read the opposite dimensions
     const int width = get_image_height(image), height = get_image_width(image);
 
-    //load current pixel value
-    region[shY][shX] = read_imagef(image, sampler, (int2)(y, x)).x;
-
-    // Load the padded regions (only edge threads)
-    if (localX == 0)
-        region[shY - 1][shX] = read_imagef(image, sampler, (int2)(y - 1, x)).x;
-    if (localX == 15)
-        region[shY + 1][shX] = read_imagef(image, sampler, (int2)(y + 1, x)).x;
-    if (localY == 0)
-        region[shY][shX - 1] = read_imagef(image, sampler, (int2)(y, x - 1)).x;
-    if (localY == 15)
-        region[shY][shX + 1] = read_imagef(image, sampler, (int2)(y, x + 1)).x;
-
-    // Load the corners of the padded region (only edge threads)
-    if (localX == 0 && localY == 0)
-        region[shY - 1][shX - 1] = read_imagef(image, sampler, (int2)(y - 1, x - 1)).x;
-    if (localX == 15 && localY == 15)
-        region[shY + 1][shX + 1] = read_imagef(image, sampler, (int2)(y + 1, x + 1)).x;
-    if (localX == 0 && localY == 15)
-        region[shY - 1][shX + 1] = read_imagef(image, sampler, (int2)(y - 1, x + 1)).x;
-    if (localX == 15 && localY == 0)
-        region[shY + 1][shX - 1] = read_imagef(image, sampler, (int2)(y + 1, x - 1)).x;
-
+    //load cooperatively the 18 x 18 region for this 16 x 16 block
+    for (int i = localId; i < 324; i += get_local_size(0) * get_local_size(1))
+    {
+        const int tileRow = i / 18;
+        const int tileCol = i % 18;
+        const int globalX =  get_group_id(1) * get_local_size(1) + tileCol - 1;
+        const int globalY = get_group_id(0) * get_local_size(0) + tileRow - 1;
+        region[tileRow][tileCol] = read_imagef(image, sampler, (int2)(globalY, globalX)).x;
+    }
     barrier(CLK_LOCAL_MEM_FENCE);
 
     //calculate the dot product of the coefficients and the neighborhood for this pixel
     if (x < width && y < height) 
     {
+        const int centerCol = get_local_id(1) + 1;
+        const int centerRow = get_local_id(0) + 1;
         float dot = 0.0f;
-        dot += coeffs[0] * region[shY - 1][shX - 1];
-        dot += coeffs[1] * region[shY - 1][shX];
-        dot += coeffs[2] * region[shY - 1][shX + 1];
-        dot += coeffs[3] * region[shY][shX - 1];
-        dot += coeffs[4] * region[shY][shX + 1];
-        dot += coeffs[5] * region[shY + 1][shX - 1];
-        dot += coeffs[6] * region[shY + 1][shX];
-        dot += coeffs[7] * region[shY + 1][shX + 1];
+        dot += coeffs[0] * region[centerRow - 1][centerCol - 1];
+        dot += coeffs[1] * region[centerRow - 1][centerCol];
+        dot += coeffs[2] * region[centerRow - 1][centerCol + 1];
+        dot += coeffs[3] * region[centerRow][centerCol - 1];
+        dot += coeffs[4] * region[centerRow][centerCol + 1];
+        dot += coeffs[5] * region[centerRow + 1][centerCol - 1];
+        dot += coeffs[6] * region[centerRow + 1][centerCol];
+        dot += coeffs[7] * region[centerRow + 1][centerCol + 1];
 		x_[(x * height + y)] = dot;
     }
 }
